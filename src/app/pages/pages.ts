a9fa9ac9cc8b4ec279f067fc9f01afe89ef6fae9
sashakhoo <sashakhoo8@gmail.com>
2023-11-17 09:31:10 +0800

    modified:   package-lock.json 	modified:   package.json 	deleted:    src/app/layout/Header.tsx 	modified:   src/app/layout/Sidebar.tsx 	modified:   src/app/pages/pages.ts 	deleted:    src/static/dark.jpg 	deleted:    src/static/light.jpg 	deleted:    src/static/screenshot.gif 	modified:   tsconfig.json

--- src/app/pages/pages.ts
+++ src/app/pages/pages.ts
@@ -4,6 +4,5 @@ export const pages = [
   { index: 2, name: 'experience.md', route: '/experience', visible: true },
   { index: 3, name: 'education.md', route: '/education', visible: true },
   { index: 4, name: 'projects.md', route: '/projects', visible: true },
-  { index: 5, name: 'certificates.md', route: '/certificates', visible: true },  
-  { index: 6, name: 'docs.md', route: '/docs', visible: false },  
+  { index: 5, name: 'docs.md', route: '/docs', visible: false },  
 ];
