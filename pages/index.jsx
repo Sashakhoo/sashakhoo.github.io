d572d527c67fef026a355f57fcc8a8b23feddb6a
sashakhoo <natasya@sns.com.my>
2023-12-29 12:15:23 +0800

    .

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,18 +2,19 @@ import Link from 'next/link';
 import Illustration from '../components/Illustration';
 import styles from '../styles/HomePage.module.css';
 
-export default function HomePage() {
+
+export default function HomePage() { 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.foreground}>
-          <div className={styles.content}>
+          <div className={styles.content}> 
             <div className={styles.centered}>
               <h1 className={styles.name}>Natasya Khoo</h1>
               <h6 className={styles.bio}>Junior Software Engineer</h6>
-              <Link href="/contact">
-                <button className={styles.outlined}>Contact Me</button>
-              </Link>
+              <a href="resume.pdf" target='_blank' download>
+                <button className={styles.outlined}>Resume</button>
+              </a>
             </div>
           </div>
           <Illustration className={styles.illustration} />
@@ -22,3 +23,5 @@ export default function HomePage() {
     </>
   );
 }
+
+
