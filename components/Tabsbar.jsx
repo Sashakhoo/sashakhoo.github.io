fda75b3f183fb849761147b384017a8c30585ba2
sashakhoo <sashakhoo8@gmail.com>
2024-05-25 14:02:15 +0800

    remove github.md

--- components/Tabsbar.jsx
+++ components/Tabsbar.jsx
@@ -6,9 +6,9 @@ const Tabsbar = () => {
     <div className={styles.tabs}>
       <Tab icon="/react_icon.svg" filename="home.jsx" path="/" />
       <Tab icon="/html_icon.svg" filename="about.html" path="/about" />
-      <Tab icon="/css_icon.svg" filename="contact.css" path="/contact" />
       <Tab icon="/js_icon.svg" filename="projects.js" path="/projects" />
-      <Tab icon="/markdown_icon.svg" filename="github.md" path="/github" />
+      <Tab icon="/js_icon.svg" filename="skills.js" path="/skills" />
+      <Tab icon="/css_icon.svg" filename="contact.css" path="/contact" />
     </div>
   );
 };
